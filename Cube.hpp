4775f79adc6e666774f89958e3cbb0693a2437c7
Cristian Machacado <cristian.machacado@intraway.com>
2021-10-16 14:28:20 -0500

    add class Cube

---
+++ Cube.hpp
@@ -0,0 +1,22 @@
+/**
+ * @author      : Cristian David (cdavidmr@$HOSTNAME)
+ * @file        : Cube
+ * @created     : sábado oct 16, 2021 14:25:27 -05
+ */
+
+#ifndef CUBE_HPP
+
+#define CUBE_HPP
+
+
+class Cube
+{
+    public:
+        Cube ();
+        virtual ~Cube ();
+    private:
+        /* private data */
+};
+
+#endif /* end of include guard CUBE_HPP */
+
