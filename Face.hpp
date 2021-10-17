9c21f9ed276b9ed7bf35f61c95c7db597b81deea
LauraR287 <81595177+LauraR287@users.noreply.github.com>
2021-10-16 21:43:34 -0500

    Update Face.hpp

--- Face.hpp
+++ Face.hpp
@@ -1,9 +1,3 @@
-/**
- * @author      : Cristian David (cdavidmr@$HOSTNAME)
- * @file        : Face
- * @created     : sábado oct 16, 2021 15:51:26 -05
- */
-
 #ifndef FACE_HPP
 #define FACE_HPP
 
@@ -11,19 +5,34 @@
 #include <vector>
 #include "Row.hpp"
 
+/*
+Se define la clase "Face" la cual permite definir las filas en cada una de ellas. 
+Gráficamente se ve así:
+
+       row_sup 
+|---------|---------|
+| sq_izq  | sq_der  |
+|---------|---------|
+
+       row_inf
+|---------|---------|
+| sq_izq  | sq_der  |
+|---------|---------| */
+
 class Face
 {
     public:
-        Face ();
-        //~Face ();
+        Face ();    //Constructor
+        //~Face ();  //Destructor
     private:
-        /* private data */
-        int color;
-        Row row_sup;
-        Row row_inf;
+        int color;    //Color que poseerá la cara
+        Row row_sup;    //Fila superior
+        Row row_inf;    //Fila inferior
 };
 
-Face::Face() {}
+Face::Face() {}     //Constructor
+
+
 
 #endif /* end of include guard FACE_HPP */
 
